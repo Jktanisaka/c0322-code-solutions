dac34bb543a2360f1fcaa364bcd4847b80fc539e
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-21 22:20:22 +0000

    completed javascript-objects exercise Jonathan Tanisaka

---
+++ javascript-objects/main.js
@@ -0,0 +1,33 @@
+var student = {
+  firstName: 'Jonathan',
+  lastName: 'Tanisaka',
+  age: 20
+};
+
+var fullName = student.firstName + ' ' + student.lastName;
+console.log('value of fullName:', fullName);
+
+student.livesInIrvine = true;
+student.previousOccupation = 'real estate marketing';
+console.log('value of livesInIrvine:', student.livesInIrvine);
+console.log('value of previousOccupation:', student.previousOccupation);
+console.log('value of student:', student);
+var vehicle = {
+  make: 'Mazda',
+  model: 'RX7',
+  year: 1993
+};
+vehicle['color'] = 'Red';
+vehicle['isConvertible'] = false;
+console.log('value of vehicle["color"]:', vehicle['color']);
+console.log('value of vehicle["isConvertible"]:', vehicle['isConvertible']);
+console.log('value of vehicle:', vehicle);
+
+var pet = {
+  name: 'Sadie',
+  type: 'Dog'
+};
+
+delete pet.name;
+delete pet.type;
+console.log('value of pet:', pet);
