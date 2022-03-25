0624b776376ef9f34ab2d3d1e69168dabf4e4f71
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-25 02:06:49 +0000

    completed javascript-loops-practice Jonathan Tanisaka

---
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -0,0 +1,8 @@
+/* exported addSuffixToAll */
+function addSuffixToAll(words, suffix) {
+  var newSuffix = [];
+  for (var i = 0; i < words.length; i++) {
+    newSuffix.push(words[i] + suffix);
+  }
+  return newSuffix;
+}
---
+++ javascript-loops-practice/filter-out-strings.js
@@ -0,0 +1,10 @@
+/* exported filterOutStrings */
+function filterOutStrings(values) {
+  var noStrings = [];
+  for (var i = 0; i < values.length; i++) {
+    if ((values[i] * 1) === values[i]) {
+      noStrings.push(values[i]);
+    }
+  }
+  return noStrings;
+}
