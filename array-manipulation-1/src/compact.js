9660f9c19bc99d00e555bfa7a8ba30697a26deb2
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-25 21:55:36 +0000

    completed array manipulation exercise Jonathan Tanisaka

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,7 +3,11 @@
 make new function with parameter array
 assign empty array to variable newArray
 use for loop to iterate through each index in the array
-if value evaluates to true, push the value to newArray
+new var i = 0, if i is less than the length of the array - run the code
+increment i by 1 at the end of each iteration
+if the boolean value evaluates to true (use Boolean()), push the value to newArray
+if it doesnt, continue the loop (no code required for this part)
+return newArray after loop is finished
 */
 function compact(array) {
   var newArray = [];
