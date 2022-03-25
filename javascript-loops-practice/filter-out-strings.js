6831ac9067b49cc8bda10c058eab41a8224a8685
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-25 19:34:36 +0000

    updated filter-out-strings to filter strings instead of return numbers

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var noStrings = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       noStrings.push(values[i]);
     }
   }
