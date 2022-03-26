b7edb892799dbfaf90ba5fca1a0c7c5827f70dfb
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-26 01:51:13 +0000

    updated object-manipulation-1 and pseudocode

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,9 +1,7 @@
 /* exported setValue */
 /*
 create a new function with the parameters object, key, and value
-create  for-in loop that goes through the object to the provided key and
-assign that key to value
-return prop
+assign the value to the key of the object
 */
 function setValue(object, key, value) {
   object[key] = value;
