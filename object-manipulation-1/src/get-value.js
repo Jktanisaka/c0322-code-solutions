ab299649749775afa86dbf7baf661ffb1e9cb782
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-26 00:20:27 +0000

    completed object-manipulation exercise Jonathan Tanisaka

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -3,11 +3,12 @@
 create new function with the parameters object and key
 use a for-in loop to iterate through the object
 use the provided key to get the corresponding value from the object
-
+set variable i to the value
+return i from the function
 */
 function getValue(object, key) {
-  for (var x in object) {
-    x = object[key];
+  for (var i in object) {
+    i = object[key];
   }
-  return x;
+  return i;
 }
--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -3,10 +3,11 @@
 create a new function with the parameters object, key, and value
 create  for-in loop that goes through the object to the provided key and
 assign that key to value
-
+return prop
 */
 function setValue(object, key, value) {
-  for (key in object) {
+  for (var prop in object) {
     object[key] = value;
   }
+  return prop;
 }
