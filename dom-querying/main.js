76e85ac2602f4e6e5b0c8343ea625e6839f2653a
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-28 17:36:16 +0000

    completed dom-querying exercise Jonathan Tanisaka

---
+++ dom-querying/main.js
@@ -0,0 +1,9 @@
+console.log('hello, world');
+console.log('h1 query', document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
+console.log('id of explanation', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+console.log('class of hint', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+console.log('console query p', document.querySelectorAll('p'));
+console.log('example link', document.querySelectorAll('.example-link'));
