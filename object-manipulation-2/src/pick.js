6b592205c4af0aeb9066f6d349d77770c64753d7
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-28 18:47:06 +0000

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,1 +1,9 @@
 /* exported pick */
+function pick(source, keys) {
+  var newObj = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (keys[i] === source.keys) {
+      return newObj;
+    }
+  }
+}
