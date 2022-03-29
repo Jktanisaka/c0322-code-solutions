600ebf48be9678511cc3197bd235d9def2c241c0
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-29 19:30:57 +0000

    completed javascript-light-bulb challenge with boolean Jonathan Tanisaka

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,25 +1,27 @@
 /*
-create variable to track # of clicks
+create variable to track the boolean state of clicks
 create variable to store the dom selector for the button
 create variable to store the dom selector for the background
 add an event listener to the button to listen for clicks and execute a new funtion
 create new funtion for button click that checks if clicks are even or odd
-if the click is even change the class names for both the button and background to the on colors
-if the click is odd, change the class names again to the off colors
-at the end of each condition, increment clicks by 1
+if the click is true change the class names for both the button and background to the on colors
+if the click is false, change the class names again to the off colors
+at the end of each condition, assign clicks the opposite boolean value
 */
-var clicks = 1;
+var clicks = false;
 var button = document.querySelector('.column25');
 var background = document.querySelector('.container');
 button.addEventListener('click', buttonClick);
 
 function buttonClick(event) {
-  if (clicks % 2 === 0) {
+  if (clicks === true) {
     button.className = 'column25 on';
     background.className = 'container container-on';
+    clicks = false;
   } else {
     button.className = 'column25 off';
     background.className = 'container container-off';
+    clicks = true;
   }
-  clicks++;
+
 }
