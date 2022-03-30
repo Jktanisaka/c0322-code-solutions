ad11b33d55d39e659330563abb97ab7554e6e105
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-03-30 22:42:02 +0000

    updated javascript forms exercise

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,10 +2,9 @@ var contact = document.querySelector('#contact-form');
 
 contact.addEventListener('submit', formHandling);
 
-var formData = {};
-
 function formHandling(event) {
   event.preventDefault();
+  var formData = {};
   var elementsName = contact.elements.name.value;
   var elementsEmail = contact.elements.email.value;
   var elementsMessage = contact.elements.message.value;
