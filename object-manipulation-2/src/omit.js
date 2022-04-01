555bf0fc7be5752f8e5f72d6d0b76e3473f6a524
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-01 03:15:43 +0000

    in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,13 @@
 /* exported omit */
+function omit(source, keys) {
+  var newObj = {};
+  newObj = Object.assign(newObj, source);
+
+  for (var i = 0; i < keys.length; i++) {
+    if (newObj[keys[i]] !== undefined) {
+      delete newObj[keys[i]];
+
+    }
+  }
+  return newObj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,21 +1,12 @@
 /* exported pick */
+
 function pick(source, keys) {
   var newObj = {};
   for (var i = 0; i < keys.length; i++) {
-    if (keys[i] in source) {
-
+    if (source[keys[i]] !== undefined) {
       newObj[keys[i]] = source[keys[i]];
 
     }
   }
   return newObj;
 }
-
-/* = 0; i < source.length; i++) {
-console.log(source[keys[i]])
-    if (keys[i] === source[keys[i]]) {
-      newObj.);
-    }
-  }
-}
-*/
