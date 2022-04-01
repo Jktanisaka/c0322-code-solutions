1f9d2b6ee12a8528336f3d332aaff22b3fdc430b
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-01 21:30:00 +0000

    in progress

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,18 @@
 /* exported invert */
+/*
+purpose: create a function that takes an object as input and will return a new object but with the keys and
+values inverted
+ create a new function with the parameter source
+ create a for in loop to iterate through every property in the object
+*/
+var newObj = {};
+function invert(source) {
+  var values = Object.values(source);
+  var keys = Object.keys(source);
+  for (var i = 0; i < values.length; i++) {
+    newObj[values[i]] = keys[i];
+
+  }
+  return newObj;
+
+}
