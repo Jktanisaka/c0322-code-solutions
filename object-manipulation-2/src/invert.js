9f38c88f656b0fa7d470cdc7454ef4071fa83d8f
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-02 18:52:25 +0000

    completed object-manipulation-2 exercise

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -3,16 +3,15 @@
 purpose: create a function that takes an object as input and will return a new object but with the keys and
 values inverted
  create a new function with the parameter source
+ create var newObj to serve as the new object with the requested key-values
  create a for in loop to iterate through every property in the object
+ in the loop assign the value as the keyname in newObj
+ once the loop is done, return newObj
 */
-var newObj = {};
 function invert(source) {
-  var values = Object.values(source);
-  var keys = Object.keys(source);
-  for (var i = 0; i < values.length; i++) {
-    newObj[values[i]] = keys[i];
-
+  var newObj = {};
+  for (var key in source) {
+    newObj[source[key]] = key;
   }
   return newObj;
-
 }
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,4 +1,13 @@
 /* exported pick */
+/*
+purpose: function that creates an object composed of the picked properties and
+returns a new object
+create new function with parameters source(object) and keys(array)
+define new variable with an empty obj (newObj)
+create a for loop to iterate through keys. if source at keys exists, add it to newObj,
+if it does not exist, do nothing
+once the loop is done, return newObj
+*/
 
 function pick(source, keys) {
   var newObj = {};
