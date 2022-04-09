0be58b31fb968d347494ee78484cdb34de3116d4
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-09 20:22:22 +0000

    in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,1 +1,81 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
+var players = [
+  {
+    name: 'Jon',
+    hand: []
+  },
+  {
+    name: 'John',
+    hand: {}
+  },
+  {
+    name: 'Jolene',
+    hand: {}
+  },
+  {
+    name: 'Joann',
+    hand: {}
+  }
+];
+
+var cards = [
+  {
+    rank: 'ace',
+    suit: 'clubs'
+  },
+  {
+    rank: 'jack',
+    suit: 'clubs'
+  },
+  {
+    rank: 'king',
+    suit: 'clubs'
+  },
+  {
+    rank: 'queen',
+    suit: 'clubs'
+  },
+  {
+    rank: '10',
+    suit: 'clubs'
+  },
+  {
+    rank: '9',
+    suit: 'clubs'
+  },
+  {
+    rank: '8',
+    suit: 'clubs'
+  },
+  {
+    rank: '7',
+    suit: 'clubs'
+  },
+  {
+    rank: '6',
+    suit: 'clubs'
+  },
+  {
+    rank: '5',
+    suit: 'clubs'
+  },
+  {
+    rank: '4',
+    suit: 'clubs'
+  },
+  {
+    rank: '3',
+    suit: 'clubs'
+  },
+  {
+    rank: '2',
+    suit: 'clubs'
+  }
+];
+
+for (var i = 0; i < players.length; i++) {
+  players[i].hand = _.sampleSize(cards, 2);
+  _.pullAll(cards, players[i].hand);
+  console.log('players hand', players[i].hand);
+}
+console.log('cards', cards);
