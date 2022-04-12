a84ec58b74d4d6f53efd6676c184a65430a7e721
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-04-12 22:25:32 +0000

    completed javascript-bank exercise

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -7,11 +7,28 @@ function Bank() {
 Bank.prototype.openAccount = function (holder, balance) {
   if (balance > 0 && Number.isInteger(balance)) {
     var newAcc = new Account(this.nextAccountNumber, holder);
-    newAcc.balance = balance;
+    newAcc.deposit(balance);
     this.accounts.push(newAcc);
     this.nextAccountNumber++;
     return newAcc.number;
   } else {
     return null;
   }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var total = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    total += this.accounts[i].getBalance();
+  }
+  return total;
 };
