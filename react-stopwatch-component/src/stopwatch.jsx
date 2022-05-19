0f53b1c0af412aabf080eb7209efcf498ed62ed9
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-05-19 03:51:34 +0000

    completed react-stopwatch-component challenge

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,23 +4,31 @@ export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = { status: 'on' };
+    this.reset = this.reset.bind(this);
+    this.state = { status: 'pause', number: 0 };
+  }
 
+  reset() {
+    this.setState({ number: 0 });
   }
 
   handleClick() {
-    if (this.state.status === 'on') {
-      this.setState({ status: 'off' });
-    }
-    if (this.state.status === 'off') {
-      this.setState({ status: 'on' });
+    if (this.state.status === 'pause') {
+      this.setState({ status: 'play' });
+      this.intervalID = setInterval(() => {
+        this.setState({ number: this.state.number + 1 });
+      }, 1000);
+    } else {
+      clearInterval(this.intervalID);
+      this.setState({ status: 'pause', number: this.state.number });
     }
   }
 
   render() {
-    const isToggled = this.state.status;
-    if (isToggled === 'on') {
-      return <div className='outer'><div className='circle'><div className='number'>1234</div></div><div className="play"><i className="fa-solid fa-play"></i></div></div>;
+    const isPlaying = this.state.status;
+    if (isPlaying === 'pause') {
+      return <div className='outer'><div className='circle' onClick={this.reset}><div className='number'>{this.state.number}</div></div><div className="play"><i className="fa-solid fa-play" onClick={this.handleClick}></i></div></div>;
     }
+    return <div className='outer'><div className='circle'><div className='number'>{this.state.number}</div></div><div className="play"><i className="fa-solid fa-pause" onClick={this.handleClick}></i></div></div>;
   }
 }
