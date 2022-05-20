255263b9c3fa0007303ba934af1bcc58dd563775
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-05-20 00:39:53 +0000

    updated state object, passwordtext, and fontawesomeclass

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -4,35 +4,35 @@ export default class ValidateInput extends React.Component {
   constructor(props) {
     super(props);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.state = { password: '', fontAwesomeClass: 'fa-solid fa-xmark' };
-    this.passwordText = 'A password is required.';
+    this.state = { password: '', passwordText: 'A password is required.' };
   }
 
   handlePasswordChange(event) {
     this.setState({ password: event.target.value });
     if (event.target.value.length === 0) {
-      this.setState({ fontAwesomeClass: 'fa-solid fa-xmark' });
-      this.passwordText = 'A password is required.';
+      this.setState({ passwordText: 'A password is required.' });
     }
     if (event.target.value.length < 8 && event.target.value.length > 0) {
-      this.setState({ fontAwesomeClass: 'fa-solid fa-xmark' });
-      this.passwordText = 'Your password is too short.';
+      this.setState({ passwordText: 'Your password is too short.' });
     }
     if (event.target.value.length >= 8) {
-      this.setState({ fontAwesomeClass: 'fa-solid fa-check' });
-      this.passwordText = '';
+      this.setState({ passwordText: null });
     }
   }
 
   render() {
+    let fontAwesomeClass = 'fa-solid fa-xmark';
+    if (!this.state.passwordText) {
+      fontAwesomeClass = 'fa-solid fa-check';
+    }
     return (
     <div>
       <h2>Password</h2>
       <div className='input-container'>
         <input type="password" value={this.state.password} onChange={this.handlePasswordChange}></input>
-          <i className={this.state.fontAwesomeClass}></i>
+          <i className={fontAwesomeClass}></i>
       </div>
-      <h5>{this.passwordText}</h5>
+      <h5>{this.state.passwordText}</h5>
     </div>
     );
   }
