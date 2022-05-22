cbbfdaf8f10490b43672583eb787ab8df4ffff47
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-05-22 01:23:28 +0000

    completed react-carousel-component

--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Carousel from './carousel';
 
-// const images = [
-//   { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' },
-//   { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png' },
-//   { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' },
-//   { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' },
-//   { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png' }
-// ];
+const images = [
+  { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' },
+  { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png' },
+  { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' },
+  { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' },
+  { link: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png' }
+];
 
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(<Carousel />);
+root.render(<Carousel images={images}/>);
