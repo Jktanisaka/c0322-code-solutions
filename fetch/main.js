9bd1c17ed552ae4614d1a43e5580bfab0d8a2b00
Jonathan Tanisaka <jktanisaka@gmail.com>
2022-05-23 18:23:08 +0000

    added catch for error

--- fetch/main.js
+++ fetch/main.js
@@ -2,8 +2,10 @@
 
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.error('Fetch failure', err));
 
 fetch('https://pokeapi.co/api/v2/pokemon/212')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(err => console.error('Fetch failure', err));
